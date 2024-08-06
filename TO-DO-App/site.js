a9b1fe8ed1c41a8f2e2db7d8a31ba0a590f39e65
Nofal71 <hafiznofal76@gmail.com>
2024-08-06 11:08:58 +0500

    JavaScript To-Do-App

--- TO-DO-App/site.js
+++ TO-DO-App/site.js
@@ -1,41 +1,39 @@
-let input = document.querySelector("#input");
-let add_button = document.querySelector("#add_button");
-let task_display = document.querySelector(".task_display");
+const input = document.getElementById("text-box");
+const list = document.getElementById("list-container");
 
-let add_task = () => {
-    let data = input.value;
-    if (data == "") {
-        input.placeholder = "Write your task here...!";
+const addTask = () => {
+    if (input.value === '') {
+        input.placeholder = 'You must have to add some task..!'
     } else {
-        let task = document.createElement("span");
-        task.innerHTML = `<p class="task_area"> <input id = "check" type="radio" > ${data} <span id="delete"> <i class="fa-solid fa-xmark"></i> </span> </p>`;
-        task_display.appendChild(task);
-        input.value = "";
-        input.placeholder = "Add more tasks here...";
-        complete_task(task, task.querySelector("#check"));
-        delete_task(task, task.querySelector("#delete"));
+        const li = document.createElement('li');
+        list.appendChild(li);
+        li.innerText = input.value;
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-}
-
-let complete_task = (task, check) => {
-    let incomplete = true;
-    check.addEventListener("click", () => {
-        if (incomplete) {
-            task.style.textDecoration = "line-through";
-            incomplete = false;
-        } else {
-            task.style.textDecoration = "none";
-            check.checked = false;
-            incomplete = true;
-        }
-    });
-}
-
-let delete_task = (task, dell) => {
-    dell.addEventListener("click", () => {
-        task.remove();
-    })
+    input.value = '';
+    saveData()
 }
 
 
-add_button.addEventListener("click", add_task);
+list.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+
+
+let saveData = () => {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+let showData = () => {
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showData();
